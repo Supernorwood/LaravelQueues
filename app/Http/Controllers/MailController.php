1792f3bd5e80fb59ae9fd04a6bae8384611a050e
Gregory Norwood <Gregoryn@summitfinances.com>
2018-10-07 10:38:27 -0400

    first commit

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Mail;
+use App\Jobs\SendOrderEmail;
+use App\Order;
+use Log;
+
+class MailController extends Controller
+{
+    public function index() {
+
+        $order = Order::findOrFail( rand(1,50) );
+        SendOrderEmail::dispatch($order);
+
+        Log::info('Dispatched order ' . $order->id);
+        return 'Dispatched order ' . $order->id;
+    }
+}
